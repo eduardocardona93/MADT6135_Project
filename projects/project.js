1dcc66c60a27e4f4da168d8aa88df730721fdc39
LinoHernandez <82666758+LinoHernandez@users.noreply.github.com>
2022-02-11 18:53:09 -0500

    new icons

--- projects/project.js
+++ projects/project.js
@@ -1,3 +1,21 @@
 (function() {
     'use strict'
+
+    var members = [
+        'Clara Water','Angel Mala','Dyvia Sri','Jose Jose','Mangarret'];
+
+    function makeUL(array) {
+        var list = document.createElement('ul');
+        members.forEach(member => {
+            var item = document.createElement('li');
+
+            item.innerText = member;
+
+            list.appendChild(item);
+        });
+    console.log(list);
+        document.getElementById('teams').appendChild(list);
+    }
+
+    makeUL(members);
 }())
